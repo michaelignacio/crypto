cf50935540eb5f73bf4bd89c7775f082814da351
Michael Ignacio <mikelignacio@gmail.com>
2019-12-18 12:18:26 +0800

    Updates

---
+++ precache-manifest.5ab80ac448462a37db4fa69cd43ca543.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb80bf91f0f5516874b5326f780e9beb",
+    "url": "/michaelignacio/crypto-news-tracker/index.html"
+  },
+  {
+    "revision": "517a5f45960bbc3241c0",
+    "url": "/michaelignacio/crypto-news-tracker/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "1d720063e53e25368054",
+    "url": "/michaelignacio/crypto-news-tracker/static/css/main.3d8ff910.chunk.css"
+  },
+  {
+    "revision": "517a5f45960bbc3241c0",
+    "url": "/michaelignacio/crypto-news-tracker/static/js/2.f359a299.chunk.js"
+  },
+  {
+    "revision": "1d720063e53e25368054",
+    "url": "/michaelignacio/crypto-news-tracker/static/js/main.e4838344.chunk.js"
+  },
+  {
+    "revision": "ca1863f45083819334d4",
+    "url": "/michaelignacio/crypto-news-tracker/static/js/runtime-main.a1e87832.js"
+  }
+]);
